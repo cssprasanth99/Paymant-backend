b67aebdc3e2bc6f4c89168f0cbcdc4acfad26960
your_name_here <cssprasanth99@gmail.com>
2024-09-16 14:16:33 +0530

    initial commit

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ dotenv.config();
 const app = express();
 const port = 5000;
 
-// Enable CORS for all origins
 app.use(cors());
+
+app.use(express.json());
 
 const keyId = process.env.keyId;
 const keySecret = process.env.keySecret;
+
+console.log(keyId, keySecret);
 
 app.get("/payment-links", async (req, res) => {
   const authHeader = `Basic ${Buffer.from(`${keyId}:${keySecret}`).toString(
@@ -31,6 +34,75 @@ app.get("/payment-links", async (req, res) => {
     res.json(response.data);
   } catch (error) {
     res.status(500).json({ error: error.message });
+  }
+});
+
+app.post("/payment-links/create", async (req, res) => {
+  const authHeader = `Basic ${Buffer.from(`${keyId}:${keySecret}`).toString(
+    "base64"
+  )}`;
+
+  const { name, amount } = req.body;
+
+  try {
+    const response = await axios.post(
+      "https://api.razorpay.com/v1/payment_links/",
+      {
+        amount: amount,
+        currency: "INR",
+        description: `Payment for ${name}`,
+        customer: {
+          name: name,
+          email: "default.email@example.com",
+          contact: "+911234567890",
+        },
+        notify: {
+          sms: true,
+          email: true,
+        },
+        reminder_enable: true,
+        notes: {
+          purpose: "For XYZ purpose",
+        },
+        callback_url: "https://example-callback-url.com/",
+        callback_method: "get",
+      },
+      {
+        headers: {
+          Authorization: authHeader,
+        },
+      }
+    );
+    res.json(response.data);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: error.response ? error.response.data : error.message });
+  }
+});
+
+app.post("/payment-links/cancel/:id", async (req, res) => {
+  const authHeader = `Basic ${Buffer.from(`${keyId}:${keySecret}`).toString(
+    "base64"
+  )}`;
+
+  const paymentLinkId = req.params.id;
+
+  try {
+    const response = await axios.post(
+      `https://api.razorpay.com/v1/payment_links/${paymentLinkId}/cancel`,
+      {},
+      {
+        headers: {
+          Authorization: authHeader,
+        },
+      }
+    );
+    res.json(response.data);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: error.response ? error.response.data : error.message });
   }
 });
 
